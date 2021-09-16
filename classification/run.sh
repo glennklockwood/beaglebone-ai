6423a039a64ac178cf3de9d081e980db72109652
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-09-15 21:44:36 -0700

    strip unnecessarily complexity from makefile

--- classification/run.sh
+++ classification/run.sh
@@ -5,5 +5,5 @@ USB_CAMERA=/dev/video1
 ti-mct-heap-check -c
 
 sudo mjpg_streamer \
-    -i "input_opencv.so -r 640x480 -d ${USB_CAMERA} --filter ./classification.tidl.so" \
-    -o "output_http.so -p 8090 -w /usr/share/mjpg-streamer/www"
+    -i "input_opencv.so -r 640x480 -d ${USB_CAMERA} --filter ./classification.so" \
+    -o "output_http.so -p 8090 -w /usr/share/mjpg-streamer/www" 2>&1 | grep -v 'Corrupt JPEG data'
