24207be029e00685c4082c2277807b5af42e589e
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-09-18 23:22:42 -0700

    generate documentation of class methods too

--- pyclassify/show_tidl_api.py
+++ pyclassify/show_tidl_api.py
@@ -11,8 +11,28 @@ import tidl
 
 print("TIDL version {}".format(tidl.__version__), end="\n\n")
 print("{:28s} | {}\n{}|{}".format("Interface", "Type", "-" * 29, "-" * 49))
-for part in dir(tidl):
-    print_cmd = "str(type(tidl.{}))".format(part)
-    print("tidl.{:23s} | {}".format(
-        part,
-        str(eval(print_cmd)).split()[-1].lstrip("'").rstrip(">'")))
+
+classes = []
+for member_name in dir(tidl):
+    if member_name.startswith("__"):
+        continue
+    member_name = ".".join([tidl.__name__, member_name])
+    member = eval(member_name)
+    member_type = str(type(member)).split()[-1].lstrip("'").rstrip(">'")
+    print("{:27s} | {}".format(member_name, member_type))
+
+    if member_type == "pybind11_builtins.pybind11_type":
+        classes.append(member)
+
+for clas in classes:
+    print("-" * 80)
+    print(clas)
+    print("-" * 80)
+    for member_name in dir(clas):
+        if member_name.startswith("__"):
+            continue
+        member_name = ".".join([clas.__module__, clas.__name__, member_name])
+        member = eval(member_name)
+        member_type = str(type(member)).split()[-1].lstrip("'").rstrip(">'")
+        print("{:50s} | {}".format(member_name, member_type))
+    print()
