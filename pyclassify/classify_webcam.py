631bdb485bac0d35a29612116d8a46f010c60bac
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-09-25 14:59:20 -0700

    code clean-up, linting, verbosity

--- pyclassify/classify_webcam.py
+++ pyclassify/classify_webcam.py
@@ -1,45 +1,41 @@
 #!/usr/bin/python3
+"""
+Copyright (c) 2019 Texas Instruments Incorporated - http://www.ti.com/
+All rights reserved.
 
-# Copyright (c) 2019 Texas Instruments Incorporated - http://www.ti.com/
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-# * Neither the name of Texas Instruments Incorporated nor the
-# names of its contributors may be used to endorse or promote products
-# derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-# THE POSSIBILITY OF SUCH DAMAGE.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of Texas Instruments Incorporated nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
 
-""" Process each frame using a single ExecutionObject.
-    Increase throughput by using multiple ExecutionObjects.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-import os
 import sys
-import argparse
+import time
 import json
-import heapq
 import logging
-import numpy as np
+import subprocess
+
+import numpy
 import cv2
 import flask
-import subprocess
 
 sys.path.append("/usr/share/ti/tidl/tidl_api")
 import tidl
@@ -49,24 +45,31 @@ import tidl
 # runtime will transparently reference them which will trigger mystery
 # segfaults if Python destroys them
 EXECUTORS = {}
+LABELS = {}
 EOPS = None
 CAMERA = None
 CONFIGURATION = None
 APP = flask.Flask(__name__)
 
+FPS_QUEUE_LEN = 10 # calculate fps over this many seconds
+MIN_CONFIDENCE = 25 # don't display labels if confidence is lower than this (%)
+
 @APP.route('/')
 def video_feed():
-    global CAMERA, CONFIGURATION
+    """Returns an mjpg stream of classified image frames.
+    """
     return flask.Response(
-        stream_camera(camera=CAMERA, configuration=CONFIGURATION),
+        stream_camera(camera=CAMERA),
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def main():
-    """Read the configuration and run the network"""
-    global CAMERA, CONFIGURATION, LABELS
+    """Initializes and launches the streaming classification app.
+    """
+    global CAMERA, CONFIGURATION, LABELS, EOPS
 
     logging.basicConfig(level=logging.INFO)
 
+    logging.info("Cleaning EVE and DSP heaps...")
     subprocess.call(['ti-mct-heap-check', '-c'])
 
     CONFIGURATION = tidl.Configuration()
@@ -76,108 +79,93 @@ def main():
 
     EOPS = init_tidl(configuration=CONFIGURATION, pipeline_depth=2)
 
-    with open('imagenet_objects.json') as json_file:
+    with open("imagenet_objects.json", "r", encoding="utf8") as json_file:
         LABELS = json.load(json_file)
 
     try:
         APP.run(host='0.0.0.0')
-    except KeyboardInterrupt:
-        free_tidl(EOPS)
+    except:
+        tidl.free_memory(EOPS)
         CAMERA.release()
+        raise
 
 def init_tidl(configuration, pipeline_depth):
-    global EXECUTORS, EOPS
+    """Initializes TIDL devices, memory, and state.
+
+    Identifies the TIDL devices that will be used and allocates sufficient
+    resources to process video frames using all of them.
+    """
+    global EOPS
 
     num_eve = tidl.Executor.get_num_devices(tidl.DeviceType.EVE)
     num_dsp = tidl.Executor.get_num_devices(tidl.DeviceType.DSP)
 
-    logging.info(
-        'Running network across {} EVEs, {} DSPs'.format(num_eve, num_dsp))
+    logging.info("Running network across %d EVEs, %d DSPs", num_eve, num_dsp)
 
-    # Heap sizes for this network determined using Configuration.showHeapStats
     configuration.param_heap_size = (3 << 20)
     configuration.network_heap_size = (20 << 20)
 
-    logging.info('TIDL API: performing one time initialization ...')
-
     # Collect all EOs from EVE and DSP executors
     eos = []
-    logging.info('TIDL API: finding EVE EOs...')
-    eve_device_ids = set([tidl.DeviceId.ID0, tidl.DeviceId.ID1, tidl.DeviceId.ID2, tidl.DeviceId.ID3][0:num_eve])
+    eve_device_ids = set([tidl.DeviceId.ID0, tidl.DeviceId.ID1,
+        tidl.DeviceId.ID2, tidl.DeviceId.ID3][0:num_eve])
     if eve_device_ids:
         EXECUTORS['eve'] = tidl.Executor(tidl.DeviceType.EVE, eve_device_ids, configuration, 1)
         for i in range(EXECUTORS['eve'].get_num_execution_objects()):
             eos.append(EXECUTORS['eve'].at(i))
 
-    logging.info('TIDL API: finding DSP EOs...')
-    dsp_device_ids = set([tidl.DeviceId.ID0, tidl.DeviceId.ID1, tidl.DeviceId.ID2, tidl.DeviceId.ID3][0:num_dsp])
+    dsp_device_ids = set([tidl.DeviceId.ID0, tidl.DeviceId.ID1,
+        tidl.DeviceId.ID2, tidl.DeviceId.ID3][0:num_dsp])
     if dsp_device_ids:
         EXECUTORS['dsp'] = tidl.Executor(tidl.DeviceType.DSP, dsp_device_ids, configuration, 1)
         for i in range(EXECUTORS['dsp'].get_num_execution_objects()):
             eos.append(EXECUTORS['dsp'].at(i))
 
     # initialize ExecutionObjectPipelines
-    logging.info('TIDL API: initializing EOPs...')
     EOPS = []
-    for j in range(pipeline_depth):
-        for i, eo in enumerate(eos):
-            EOPS.append(tidl.ExecutionObjectPipeline([eo]))
+    for _ in range(pipeline_depth):
+        for i, execobj in enumerate(eos):
+            EOPS.append(tidl.ExecutionObjectPipeline([execobj]))
 
-    logging.info('TIDL API: allocating memory for EOPs...')
     tidl.allocate_memory(EOPS)
 
     return EOPS
 
-def finalize_tidl():
-    global EOPS
-    tidl.free_memory(EOPS)
+def stream_camera(camera):
+    """Classifies image from a webcam, labels them, and yields them.
 
-def device_name_to_eo(device_name):
-    """Translates device name to an ExecutionObject.
-    
-    Receives the string output from ExecutionObjectPipeline.get_device_name(),
-    finds the ExecutionObject with the same name, and returns it.  Expects only
-    device_names of the form EVE# or DSP#.
-
-    Args:
-        device_name (str): EVE0, EVE1, EVE2, EVE3, DSP0, or DSP1.
-
-    Returns:
-        tidl.ExecutionObject or None.
+    Master loop that reads an image from a camera, runs it through our model,
+    paints the top label on the image, and returns it as a component of an mjpg
+    stream.
     """
-    device_type = device_name[0:3].lower()
-    executor = EXECUTORS.get(device_type)
-    if not executor:
-        raise RuntimeError("Executors not yet initialized")
-
-    for i in range(executor.get_num_execution_objects()):
-        this_eo = executor.at(i)
-        this_device = this_eo.get_device_name()
-        if this_device.lower() == device_name.lower():
-            return this_eo 
-
-    return None
-
-def stream_camera(camera, configuration):
-    """ Run the network on the specified device type and number of devices"""
-    global LABELS
-
-    # initialize TIDL, devices, etc
     num_eops = len(EOPS)
+    logging.info('Processing using %d ExecutionObjects', num_eops)
     frames = [None] * num_eops
-
-    # initialize camera
+    timing_start = time.time()
+    tot_frames = 0
 
     frame_index = 0
     while True:
-        frame_index = frame_index % num_eops
+        frame_index = (frame_index + 1) % num_eops
         eop = EOPS[frame_index]
 
         # once an EOP becomes available, process its output
         if eop.process_frame_wait():
-            labels = process_output(eop, LABELS)
-            if len(labels) > 0 and labels[0][1] > 10.0:
-                text = "{} {:.0f}%".format(*labels[0]).replace("_", " ")
+            # calculate framerate
+            now = time.time()
+            elapsed = now - timing_start
+            if elapsed > FPS_QUEUE_LEN:
+                fps = tot_frames / elapsed
+                logging.info("Inference rate: %.0f fps", fps)
+                tot_frames = 0
+                timing_start = now
+
+            # get the most confidence classification
+            label, confidence = get_classification(eop, LABELS)
+            tot_frames += 1
+            if confidence >= MIN_CONFIDENCE:
+                # overlay the classification and confidence on the image
+                text = label.replace("_", " ") + f" {confidence:.0f}%"
                 cv2.putText(
                     img=frames[frame_index],
                     text=text,
@@ -186,66 +174,62 @@ def stream_camera(camera, configuration):
                     fontScale=1.0,
                     color=(0, 255, 0),
                     thickness=3)
+
+            # send the next frame to the mjpg stream
             _, encoded_frame = cv2.imencode(".jpg", frames[frame_index])
             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n'
                 + bytes(encoded_frame) + b'\r\n')
 
-        # load and process next frame
-        frames[frame_index] = read_frame(camera, eop, configuration)
+        # load and start processing the next frame
+        frames[frame_index] = read_frame(camera, eop)
         if frames[frame_index] is not None:
             eop.process_frame_start_async()
 
+def read_frame(camera, eop):
+    """Reads a frame into an ExecutionObject input buffer.
 
-def read_frame(camera, eop, configuration):
-    """Read a frame into the ExecutionObject input buffer"""
+    Reads a frame from the webcam, resizes it to the dimensions expected by the
+    model, and rearranges its red/green/blue channel layout in memory to match
+    what TIDL expects.  See the following for more information on the TIDL
+    tensor format.
 
-    # Read into the EO's input buffer
-    arg_info = eop.get_input_buffer()
-    np_arg = np.asarray(arg_info)
+    https://software-dl.ti.com/jacinto7/esd/processor-sdk-rtos-jacinto7/06_02_00_21/exports/docs/tidl_j7_01_01_00_10/ti_dl/docs/user_guide_html/md_tidl_fsg_io_tensors_format.html
+
+    Returns:
+        numpy.array: Unmodified frame retrieved from webcam.
+    """
 
     success, frame = camera.read()
     if not success:
         return None
 
+    # resize the frame to fit our TIDL input buffer
     resized = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # split out the channels because of the way TIDL expects images
     b_frame, g_frame, r_frame = cv2.split(resized)
-    np_arg[0*224*224:1*224*224] = np.reshape(b_frame, 224*224)
-    np_arg[1*224*224:2*224*224] = np.reshape(g_frame, 224*224)
-    np_arg[2*224*224:3*224*224] = np.reshape(r_frame, 224*224)
+
+    # np_arg becomes a pointer to a buffer that looks like a numpy array
+    np_arg = numpy.asarray(eop.get_input_buffer())
+    np_arg[0*224*224:1*224*224] = numpy.reshape(b_frame, 224*224)
+    np_arg[1*224*224:2*224*224] = numpy.reshape(g_frame, 224*224)
+    np_arg[2*224*224:3*224*224] = numpy.reshape(r_frame, 224*224)
 
     return frame
 
-def process_output(eop, labels_data):
-    """Display the inference result using labels."""
+def get_classification(eop, labels_data):
+    """Retrieves and decodes model output.
 
-    # keep top k predictions in heap
-    k = 5
+    Returns the label and confidence of the most-confident label identified by
+    the model.
 
-    # output predictions with probability of 10/255 or higher
-    threshold = 10
-
-    out_buffer = eop.get_output_buffer()
-    output_array = np.asarray(out_buffer)
-
-    k_heap = []
-    for i in range(k):
-        heapq.heappush(k_heap, (output_array[i], i))
-
-    for i in range(k, out_buffer.size()):
-        if output_array[i] > k_heap[0][0]:
-            heapq.heappushpop(k_heap, (output_array[i], i))
-
-    k_sorted = []
-    for i in range(k):
-        k_sorted.insert(0, heapq.heappop(k_heap))
-
-    results = []
-    for i in range(k):
-        if k_sorted[i][0] > threshold:
-            results.append((
-                labels_data['objects'][k_sorted[i][1]]['label'],
-                k_sorted[i][0]/255.0*100))
-    return results
+    Returns:
+        tuple of str, float: label and confidence expressed between 0 and 100.
+    """
+    # values of output_array are 8 bits of confidence per label
+    output_array = numpy.asarray(eop.get_output_buffer())
+    best_label = numpy.argmax(output_array)
+    return (labels_data['objects'][best_label]['label'], 100 * output_array[best_label] / 255)
 
 if __name__ == '__main__':
     main()
